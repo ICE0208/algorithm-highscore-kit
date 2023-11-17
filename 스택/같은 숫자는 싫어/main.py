00e9dabf1610dcf76ebc014f1da78e6c43e6ba2c
ICE0208 <doky3307@naver.com>
2023-11-18 00:25:20 +0900

    ✅ Clear

---
+++ 스택/같은 숫자는 싫어/main.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    stack = []
+    for n in arr:
+        if stack:
+            if stack[-1] != n:
+                stack.append(n)
+        else:
+            stack.append(n)
+
+    return stack
